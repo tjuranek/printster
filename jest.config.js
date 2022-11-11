1e19a1bc4a7a15900288c648482d0bfc99fc5109
Thomas Juranek <thjurane@amazon.com>
2022-11-11 12:21:56 -0700

    change jest to use @swc/jest instead of ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/app/$1'
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
   }
 };
